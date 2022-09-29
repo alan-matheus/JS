20e822404c80367cc01946566f95ce11c218b961
alanmrock <matheus10_11@hotmail.com>
2022-09-29 16:58:31 -0300

    adicionei aula11

---
+++ aulas/aula11/ex08.js
@@ -0,0 +1,7 @@
+var vel= 80
+
+console.log(`a velocidade do seu carro é ${vel} km/h `)
+if(vel > 60 ){
+    console.log('você ultrapassou a velocidade permitida: multado')
+}
+console.log('diriga sempre usando cinto de segurança')
---
+++ aulas/aula11/ex09.js
@@ -0,0 +1,8 @@
+var pais = "Uruguai"
+
+console.log("você nasceu no"+ pais)
+if(pais == "Brasil"){
+    console.log('Você é brasileiro')
+} else {
+    console.log('Você é estrangeiro')
+}
