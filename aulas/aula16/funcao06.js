f60e36ddb712a9c4210d8ef05bebe71c633991f1
Alan Matheus <matheus10_11@hotmail.com>
2023-01-16 19:55:48 -0300

    adicionei funções básicas

---
+++ aulas/aula16/funcao06.js
@@ -0,0 +1,11 @@
+
+function positivo(n){
+    if(n>0){
+        return 'Número positivo'
+    } else {
+        return 'Número negativo'
+    }
+}
+
+
+console.log(positivo(-5))
---
+++ aulas/aula16/funcao07.js
@@ -0,0 +1,7 @@
+function multiplicacao(n1, n2){
+    return n1 * n2
+}
+
+let a = multiplicacao(5, 8)
+
+console.log('O resultado da multiplicação é: '+ a)
